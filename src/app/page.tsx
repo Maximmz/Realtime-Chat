cd1e38a399db97e6ffbdf73be7c36b5dbbece3da
Maximmz <108608264+Maximmz@users.noreply.github.com>
2024-03-19 23:27:20 +0500

    Connected PostGreSQL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import { FC } from 'react'
 import { Button } from "@/components/ui/Button"
-import { db } from '@/lib/db'
 
 
 export default async function Home() {
-  await db.set("hello", "Db is working 100%")
   return (
-    <div className="">
-    <div className="mx-auto overflow-hidden">
+    <div className="max-w-7xl flex justify-center flex-col">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. A perspiciatis, aliquid voluptatem fugit assumenda expedita enim quos, consequatur doloribus aliquam accusantium necessitatibus eius? Mollitia tempore, ad nesciunt molestiae unde distinctio repellat similique quia doloribus voluptate architecto ducimus quaerat ex in pariatur sunt, eum possimus rem!
     <br></br>
-    <Button variant="default" className=""> Average button </Button>
+    <Button variant="default" className="max-w-48 lg:max-w-full"> Average button </Button>
     </div>
-  </div>
   )
 }
